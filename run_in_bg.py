55c22ae09df052af96acb8ff29c1657cbc7cf97e
Daniel Korat <daniel.korat@intel.com>
2020-06-18 15:10:45 +0300

    remove files

--- run_in_bg.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from time import strftime
-from datetime import date
-timestamp = date.today().strftime("%A_") + strftime("%H:%M:%S")
-
-subprocess.run("nohup python /home/daniel_nlp/Lung-Cancer-Risk-Prediction/main.py > out_" + timestamp + ".log 2>&1 &", shell=True)
--- run_prp_in_bg.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from time import time
-from time import strftime
-from datetime import date
-timestamp = date.today().strftime("%A_") + strftime("%H:%M:%S")
-
-subprocess.run("nohup /usr/bin/python3.6 /home/daniel_nlp/Lung-Cancer-Risk-Prediction/preprocess.py > .out_preprocess_" + timestamp + ".log 2>&1 &", shell=True)
